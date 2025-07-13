74471a76db08fd4463ad08c2aecba4f3dac097b4
Gautam Naugai <gautamnogai@gmail.com>
2025-07-13 11:53:12 +0530

    datatype commit

---
+++ datatype.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 100;
+        echo "The value of a is: ".$a. "<br>";
+
+        $b = "Hello";
+        echo "The value of b is: ".$b. "<br>";
+
+        $c = 20.96;
+        echo "The value of c is: ".$c. "<br>";
+
+        $d = array("berlin", "professor", "Tokyo");
+        print_r($d);
+
+        $e = TRUE;
+        echo "<br> The value of e is: ".$e. "<br>";
+
+        $f = NULL;
+        echo "The value of f is: ".$f;
+    ?>
+</body>
+</html>
