8b30a36760a7c813edaa60a3cf2b8533e4f95c68
Gautam Naugai <gautamnogai@gmail.com>
2025-07-13 11:31:19 +0530

    phplearning

---
+++ start.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <marquee direction="left">This is my first time for learning PHP</marquee>
+    
+    <?php
+    $n = 100;
+    $x = 100;
+    $c = $n + $x;
+    echo "The value of ".$n." and ".$x." is: ".$c;
+    ?>
+</body>
+</html>
